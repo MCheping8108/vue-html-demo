cf84ae8e03262cca36117dc1ba708fb3b53c4c1d
MCheping8108 <liuyiming53@outlook.com>
2023-10-28 07:30:48 +0800

    新增onclick

---
+++ src/views/onclick.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="click" onclick="alert('hello')"><h1>点我👈</h1></div>
+    
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+    // var click = document.getElementById("click");
+    // click.onclick = function () {
+    //     alert("hello");
+    //     return;
+    // }
+</script>
